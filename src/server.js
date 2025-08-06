16e704bbe662432e2e3648f0b844a1c2f5174069
Darak0751 <darak0751@gmail.com>
2025-08-06 21:17:08 +0300

    hw2

--- src/server.js
+++ src/server.js
@@ -12,7 +12,6 @@ export const setupServer = () => {
   app.use(express.json());
 
  app.use('/contacts', contactsRouter);
-app.use('/api/contacts', contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
