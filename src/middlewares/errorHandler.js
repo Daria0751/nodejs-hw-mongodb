36f3adcf703356425fb7ba72eba430fe6eac1fc5
Darak0751 <darak0751@gmail.com>
2025-08-09 20:50:01 +0300

    hw3

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+export const errorHandler = (err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    status: err.status || 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
+/* eslint-enable no-unused-vars */
