a9b5afdcb630d1d205b10dab2afab0313b2df5a7
Darak0751 <darak0751@gmail.com>
2025-08-11 20:59:55 +0300

    hw3

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,19 @@
+import createHttpError from 'http-errors';
+
+const { HttpError } = createHttpError;
+
 export const errorHandler = (err, req, res, _next) => {
-  const status = err.status || 500;
-  res.status(status).json({
-    status,
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
     message: 'Something went wrong',
     data: err.message,
   });
