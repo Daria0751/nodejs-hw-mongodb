e683ed2d18a240818f52583cfbe43dd666ac1d03
Darak0751 <darak0751@gmail.com>
2025-08-17 18:03:34 +0300

    hw4

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,14 +7,17 @@ import {
   handleDeleteContact,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../utils/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { createContactSchema, updateContactSchema } from '../schemas/contactSchemas.js';
 
 const router = express.Router();
 
-router.get('/', ctrlWrapper(handleGetContacts));
-router.get('/:contactId', ctrlWrapper(handleGetContactById));
-router.post('/', ctrlWrapper(handleCreateContact));
-router.patch('/:contactId', ctrlWrapper(handlePatchContact));
-router.delete('/:contactId', ctrlWrapper(handleDeleteContact));
+router.get('/',         ctrlWrapper(handleGetContacts));
+router.get('/:contactId', isValidId, ctrlWrapper(handleGetContactById));
+router.post('/',          validateBody(createContactSchema),     ctrlWrapper(handleCreateContact));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(handlePatchContact));
+router.delete('/:contactId', isValidId, ctrlWrapper(handleDeleteContact));
 
 export default router;
 
